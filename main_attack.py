01da5faa27f0cddcab586445280eed6de0de935a
Rajjyoti07 <rajjdad@gmail.com>
2026-05-10 23:27:45 +0530

    added my latest runs emnist cnn iid and non iid

--- main_attack.py
+++ main_attack.py
@@ -16,15 +16,15 @@ NUM_CLIENTS        = 10
 NUM_ROUNDS         = 20
 BATCH_SIZE         = 32
 DATASET            = "EMNIST"    # "MNIST" or "EMNIST"
-MODEL              = "MLP"      # "MLP"   or "CNN"
+MODEL              = "CNN"      # "MLP"   or "CNN"
 NUM_CLASSES        = 62         # 10 for MNIST, 62 for EMNIST
-PARTITION          = "noniid"      # "iid"   or "noniid"
+PARTITION          = "iid"      # "iid"   or "noniid"
 SEED               = 42
 DEVICE             = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Attack config
-NUM_MALICIOUS      = 1          # number of malicious clients (try 1, 3, 5)
-ATTACK_TYPE        = "targeted" # "targeted" or "random"
+NUM_MALICIOUS      = 5          # number of malicious clients (try 1, 3, 5)
+ATTACK_TYPE        = "random" # "targeted" or "random"
 SOURCE_LABEL       = 7          # label to flip FROM (targeted only)
 TARGET_LABEL       = 1          # label to flip TO   (targeted only)
 
@@ -35,7 +35,7 @@ TARGET_LABEL       = 1          # label to flip TO   (targeted only)
 #Save only for mnist_mlp_iid_attack_targeted_1, mnist_mlp_iid_attack_targeted_3 mnist_mlp_iid_attack_targeted_5
 # mnist_cnn_iid_attack_targeted_3 , and also for the EMNIST varients of these runs. 
 #----------------------------------------------
-SAVE_MODEL         = False
+SAVE_MODEL         =False
 
 print(f"Using device: {DEVICE}")
 print(f"Dataset: {DATASET} | Model: {MODEL} | Partition: {PARTITION} | Clients: {NUM_CLIENTS} | Rounds: {NUM_ROUNDS}")
